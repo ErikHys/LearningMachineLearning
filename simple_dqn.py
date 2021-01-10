39ef89b4dcfa5482dcda808636eb97f90e97ae8d
ErikHys <E.Hystad@student.uib.no>
2021-01-10 20:54:44 +0100

    Initial commit

---
+++ simple_dqn.py
@@ -0,0 +1,21 @@
+from keras.layers import Dense, Activation
+from keras.models import Sequential, load_model
+import numpy as np
+
+
+class ReplayBuffer(object):
+    def __init__(self, max_size, input_shape, n_actions, discrete=False):
+        self.mem_size = max_size
+        self.input_shape = input_shape
+        self.discrete = discrete
+        self.state_memory = np.zeros((self.mem_size, input_shape))
+        self.new_state_memory = np.zeros((self.mem_size, input_shape))
+        dtype = np.int8 if self.discrete else np.float32
+        self.action_memory = np.zeros((self.mem_size, n_actions))
+        self.reward_memory = np.zeros(self.mem_size)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.float32)
+
+    def store_transition(self, state, action, reward, state_, done):
+        index = self.mem_cntr % self.mem_size
+        self.state_memory[index] = state
+        self.new_state_memory[index] = state_
